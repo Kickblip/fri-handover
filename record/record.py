322b87def11f8897147cd00441c87100b88e2266
Kickblip <wyatt.zilker@gmail.com>
2025-09-17 16:45:19 -0500

    prompter fixes

--- record/record.py
+++ record/record.py
@@ -9,7 +9,7 @@ BLUE = (30, 58, 138)
 GRAY = (34, 34, 34)
 
 def main():
-    prompter = PrompterClient()
+    
     confirm = " "
     while confirm != 'y':
         print("Hello! Please enter your actual names.")
@@ -22,31 +22,31 @@ def main():
         confirm = input("Are these settings correct? (y/n): ").strip().lower()
 
     print("Starting recording sequence\n")
-    prompter.show("Get into position!", GRAY); time.sleep(5);
-    prompter.show("Starting in 3", RED); time.sleep(1);
-    prompter.show("Starting in 2", RED); time.sleep(1);
-    prompter.show("Starting in 1", RED); time.sleep(1);
+    prompter = PrompterClient()
+    prompter.show("Get into position!", GRAY); time.sleep(5)
+    prompter.show("Starting in 3", RED); time.sleep(1)
+    prompter.show("Starting in 2", RED); time.sleep(1)
+    prompter.show("Starting in 1", RED); time.sleep(1)
     
     kinect_client = KinectClient()
     try:             
         for trial in range(1, n_trials + 1):
             print(f"Trial {trial} of {n_trials}")
-            prompter.show(f"Trial {trial} of {n_trials}", RED); time.sleep(1);
+            prompter.show(f"Trial {trial} of {n_trials}", RED); time.sleep(2.5)
 
             if trial % 2 != 0:
-                prompter.show(f"{user1} hands to {user2}", RED); time.sleep(1);
+                prompter.show(f"{user1} hands to {user2}", RED); time.sleep(1)
                 prompter.show("Start!", GREEN)
                 kinect_client.start_recording(path=f"{trial}_{user1}_{user2}.mkv", n_seconds=5)
             else:
-                prompter.show(f"{user2} hands to {user1}", RED); time.sleep(1);
+                prompter.show(f"{user2} hands to {user1}", RED); time.sleep(1)
                 prompter.show("Start!", GREEN)
                 kinect_client.start_recording(path=f"{trial}_{user2}_{user1}.mkv", n_seconds=5)
 
-            prompter.show("Done!", BLUE); time.sleep(0.5);
+            prompter.show("Done!", BLUE); time.sleep(0.5)
         
-        prompter.show("Recording complete", GRAY); time.sleep(1);
+        prompter.show("Recording complete", GRAY); time.sleep(1)
         print("Recording complete. Thank you!")
-        kinect_client.close()
         prompter.close()
         
     except KeyboardInterrupt:
